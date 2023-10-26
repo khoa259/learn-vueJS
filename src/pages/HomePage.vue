780f99dd39b59dbb3f17b8e867516027e9e1c8f1
khoa-nguyen <khoa10688@gmail.com>
2023-10-26 10:29:34 +0700

    first-commit

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 class="title-page">Home Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+};
+</script>
+
+<style></style>
---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import HomePage from "../pages/HomePage.vue";
+import Contact from "../pages/Contact.vue";
+import About from "../pages/About.vue";
+import Posts from "../pages/Posts.vue";
+import { createRouter, createWebHistory } from "vue-router";
+export const routes = [
+  { path: "/", component: HomePage, label: "Trang chủ" },
+  { path: "/about", component: About, label: "Giới thiệu" },
+  { path: "/contact", component: Contact, label: "Liên hệ" },
+  { path: "/posts", component: Posts, label: "Thêm bài viết" },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
